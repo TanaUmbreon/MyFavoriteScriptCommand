5f78a39218387a99371305c05912ea58e1892c8e
Tana <finaltruth.shintai@gmail.com>
2018-06-23 23:59:39 +0900

    * usingを整理。

--- MyFavoriteScriptCommand/Core/SceneLoop.cs
+++ MyFavoriteScriptCommand/Core/SceneLoop.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyFavoriteScriptCommand.Core
+﻿namespace MyFavoriteScriptCommand.Core
 {
     /// <summary>
     /// シーン実行ループを提供します。
