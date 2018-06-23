0c86b84d9d1b3e6a2c112002a173a315075d0d91
Tana <finaltruth.shintai@gmail.com>
2018-06-23 23:54:24 +0900

    * シーンの遷移をStateパターンで置き換えるために必要な要素を追加。

---
+++ MyFavoriteScriptCommand/Core/ISceneContext.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFavoriteScriptCommand.Core
+{
+    /// <summary>
+    /// シーンの実行環境を提供します。
+    /// </summary>
+    public interface ISceneContext
+    {
+        /// <summary>
+        /// 現在のシーンを取得または設定します。
+        /// </summary>
+        IScene CurrentScene { get; set; }
+    }
+}
---
+++ MyFavoriteScriptCommand/Core/SceneLoop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavoriteScriptCommand.Core
+{
+    /// <summary>
+    /// シーン実行ループを提供します。
+    /// </summary>
+    public class SceneLoop : ISceneContext
+    {
+        /// <summary>
+        /// 現在のシーンを取得または設定します。
+        /// </summary>
+        public IScene CurrentScene { get; set; } = null;
+
+        /// <summary>
+        /// <see cref="SceneLoop"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public SceneLoop() { }
+
+        /// <summary>
+        /// シーン実行ループを実行します。
+        /// </summary>
+        public void Run()
+        {
+            while (CurrentScene != null && !CurrentScene.CanExit)
+            {
+                CurrentScene.Run(this);
+            }
+        }
+    }
+}
