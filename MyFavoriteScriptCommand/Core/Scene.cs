f5a7390452184f8dd27905df1fc0028abb83273e
Tana <finaltruth.shintai@gmail.com>
2018-06-30 23:44:55 +0900

    * ISceneインターフェースの共通部分を実装した抽象クラスを追加。

---
+++ MyFavoriteScriptCommand/Core/Scene.cs
@@ -0,0 +1,20 @@
+﻿namespace MyFavoriteScriptCommand.Core
+{
+    /// <summary>
+    /// ゲームのシーンを表す基底クラスです。
+    /// <see cref="IScene"/> インターフェース
+    /// </summary>
+    public abstract class Scene : IScene
+    {
+        /// <summary>
+        /// シーン実行ループを抜けてアプリケーションを終了できること示す値を取得します。
+        /// </summary>
+        public bool CanExit { get; set; } = false;
+
+        /// <summary>
+        /// シーンを実行します。
+        /// </summary>
+        /// <param name="context">シーンの実行環境。</param>
+        public abstract void Run(ISceneContext context);
+    }
+}
